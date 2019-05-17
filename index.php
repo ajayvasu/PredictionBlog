75c1f472a813c55ecf1e5ef3847328d35b7c9ff4
Ajay Vasu <avasu@mtu.edu>
2019-05-17 15:39:00 -0700

    Created initial files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php
+ echo "data";
+ ?>
+
+ <html lang="en">
+ <head>
+   <title>Future Blog</title>
+   <meta charset="utf-8">
+   <link href="style/indexStyle.css" rel="stylesheet" type="text/css">
+ </head>
+
+ <body>
+   <p>This is a line of text</p>
+ </body>
+
+
+ </html>
