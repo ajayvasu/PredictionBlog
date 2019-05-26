e3007cbaa65a5c4cecd04442f69ee7cf6e5be7a8
Ajay Vasu <avasu@mtu.edu>
2019-05-25 22:26:15 -0700

    reogranized files

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <!DOCTYPE html>
-<?php
-
-if($_POST["username"] == "apple" && $_POST["password"] == "123"){
-  $servername = "localhost";
-  $username = "root";
-  $password = "1234567890";
-
-  // Create connection
-  $conn = new mysqli($servername, $username, $password);
-
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  echo "Connected successfully";
-}else{
-  header('Location: http://127.0.0.1:8081/src/login.php');
-}
-
-?>
 
  <html lang="en">
  <head>
@@ -28,19 +8,15 @@ if($_POST["username"] == "apple" && $_POST["password"] == "123"){
  </head>
 
  <body>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Predictions</a></li>
-        <li><a href="#">Opinions</a></li>
-        <li><a href="#">Discussions</a></li>
-        <li><a href="#">Journal</a></li>
-        <li><a href="src/login.php">Login</a></li>
-    </ul>
-    <div id="contentBox">
-       <p>Login to view stories</p>
-    </div>
+   <h1>Stories about possible futures.</h1>
+   <p>Login for access to content.</p>
+   <form action="../src/home.php" method="post"><br>
+     <input name="username" type="text" placeholder="username"><br>
+     <input name="password" type="password" placeholder="password"><br>
+     <input type="submit">
+   </form>
+
 
  </body>
-
 
  </html>
---
+++ src/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<?php
+
+if($_POST["username"] == "apple" && $_POST["password"] == "123"){
+  $servername = "localhost";
+  $username = "root";
+  $password = "1234567890";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+}else{
+  header('Location: http://127.0.0.1:8081/index.php');
+}
+
+?>
+
+ <html lang="en">
+ <head>
+   <title>Future Blog</title>
+   <meta charset="utf-8">
+   <link href="../style/homeStyle.css" rel="stylesheet" type="text/css">
+ </head>
+
+ <body>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Predictions</a></li>
+        <li><a href="#">Opinions</a></li>
+        <li><a href="#">Discussions</a></li>
+        <li><a href="#">Journal</a></li>
+    </ul>
+    <div id="contentBox">
+
+    </div>
+
+ </body>
+
+
+ </html>
